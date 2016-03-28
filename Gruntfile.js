361c7bfec45237d99ed13d1004b1490bddf5885b
Bruna <brunas.dev@gmail.com>
2016-03-28 14:49:16 -0300

    init config tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,5 +2,25 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		// Tasks que o grunt vai rodar.
+
+		uglify: {
+			options: {
+				mangle: false
+			},
+
+			task: {
+				files: {
+					'assets/js/main.js' : ['source/js/script.js'] 
+				}
+			}
+		} // uglify
+
 	});
+
+		// plugins do grunt
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+		// tarefas que serão executadas
+	grunt.registerTask( 'default', [ 'uglify' ] );
+
 };
