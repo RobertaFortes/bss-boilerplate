0ffb6d8207308ecc87975fd290f42a383e5bb3c2
Bruna <brunas.dev@gmail.com>
2016-03-29 19:07:40 -0300

    plugin sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+	require('load-grunt-tasks')(grunt); // plugin load tasks on package.json
+
 	grunt.initConfig({
 		// Tasks que o grunt vai rodar.
 
@@ -13,14 +15,21 @@ module.exports = function(grunt) {
 					'assets/js/main.js' : ['source/js/script.js'] 
 				}
 			}
-		} // uglify
+		}, // uglify
 
+		sass: {
+			options: {
+				style: 'compressed'
+			},
+
+			task: {
+				files: {
+					'assets/css/main.css' : 'source/sass/style.scss'
+				}
+			}
+		}, // sass 
 	});
 
-		// plugins do grunt
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-
 		// tarefas que serão executadas
-	grunt.registerTask( 'default', [ 'uglify' ] );
-
+	grunt.registerTask( 'default', [ 'uglify' , 'sass'] );
 };
